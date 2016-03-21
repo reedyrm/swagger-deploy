b3f126b17108d5fdb0b7014058889924b9c5398e
Richard Reedy <richard_reedy@volusion.com>
2016-03-21 10:11:38 -0500

    added constants as exported field

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 "use strict";
 let deployUtils = require('./utils/deployUtils.js');
+let environmentConstants = require('./constants.js');
 
 
 module.exports = function() {
   return {
     deployUtils: deployUtils,
-    deployUtilsClass: deployUtils
-  }
+    deployUtilsClass: deployUtils,
+    constants: environmentConstants
+  };
 }();
